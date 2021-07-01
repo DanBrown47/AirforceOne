9d22f0cccd437c6695648d7e4adf80f927311a32
DanBrown47 <danwand47@gmail.com>
2021-07-01 14:54:45 +0530

    Few updates

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ Author Danwand NS | admin@danwand.me
 
 
 Inspired from Bash Script by [Vishnu053](https://github.com/Vishnu053)
+
+
+Todo Stuff
+ 
+--  Structured stuff
+--  Dotenv
+--  build version | from package.json
+-- .pem checker  | path
+-- .pem permission checker 
+-- .git checker
---
+++ core/backup.py
@@ -0,0 +1,20 @@
+
+import os #Executing os commands directly
+class main():
+    def runUpdate(server_name, server_ip, pem_file_name):
+        whoishe = (str(server_name)+"@"+str(server_ip))
+        '''
+        The core function of the script
+        '''
+        print("Updating the server "+ str(server_ip)+" with new build\n")
+        print("Taking backup of the current build\n")
+        # Take a backup of the current build
+        os.system(f"ssh -i {pem_file_name} -t {whoishe} 'sudo zip -r client_bkp.zip /srv/client-folder/www'")
+        print("backedup to /srv/client-folder/www\n")
+        # Copy the new build files
+        os.system(f"scp -r -i {pem_file_name} client_build.zip/ {whoishe}:~/")
+        # Stopping nginx in server and replace the files
+        os.system(f"ssh -i {pem_file_name} -t {whoishe} 'sudo systemctl stop nginx.service'")
+        os.system(f"ssh -i {pem_file_name} -t {whoishe} 'sudo unzip ~/client_build.zip -d /srv/client-folder/www'")
+        # Start nginx
+        os.system(f"ssh -i {pem_file_name} -t {whoishe} 'sudo systemctl start nginx.service'")
